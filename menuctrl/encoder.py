01af7c022a7f337eb62b375901b677f6c0669cbc
RaphaelMaschinsen <raphaelmijnssen1@yahoo.de>
2023-10-20 17:08:20 +0200

    gpiod test added

--- menuctrl/encoder.py
+++ menuctrl/encoder.py
@@ -1,5 +1,13 @@
 from .gpio import AuriliaGPIO
+import gpiod
+import select
+
+
+GPIO_CHIP = "/dev/gpiochip0"
+INPUT = 25
+
 
 def test():
     gpio = AuriliaGPIO()
+    gpio.add_event_detect()
     gpio.add_event_detect()
--- menuctrl/gpio.py
+++ menuctrl/gpio.py
@@ -1,7 +1,16 @@
+import time
+import threading
+
+
 class AuriliaGPIO:
-    def __init__(self, pin_number=25):
-        self.pin_number = pin_number
-        print("init")
+    def __init__(self, name="TestString"):
+        self.name = name
 
     def add_event_detect(self):
-        print(self.pin_number)
+        thread = threading.Thread(target=self.pin_interrupt_task)
+        thread.start()
+
+    def pin_interrupt_task(self):
+        while True:
+            print(self.name)
+            time.sleep(1)

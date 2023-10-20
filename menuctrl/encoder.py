5f9ad3b63871e677687d7f62ec817a36333c596d
RaphaelMaschinsen <raphaelmijnssen1@yahoo.de>
2023-10-20 18:26:15 +0200

    a

--- menuctrl/encoder.py
+++ menuctrl/encoder.py
@@ -5,11 +5,12 @@ from gpiod.line import Bias, Edge
 def task1():
     print("switch callback")
 
+
 def task2():
     print("rotary callback")
 
 
 def test():
-    GPIO.add_event_detect(25, Edge.FALLING, task1)
-    GPIO.add_event_detect(14, Edge.FALLING, task2)
-    GPIO.add_event_detect(26, Edge.FALLING, task2)
+    GPIO.add_event_detect(25, Edge.FALLING, task1, bouncetime=10)
+    GPIO.add_event_detect(14, Edge.BOTH, task2)
+    GPIO.add_event_detect(26, Edge.BOTH, task2)

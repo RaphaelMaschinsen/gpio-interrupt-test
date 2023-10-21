9be0875f216ebabf9bc3a7f038ad93e4dd470adf
RaphaelMaschinsen <raphaelmijnssen1@yahoo.de>
2023-10-21 13:21:17 +0200

    a

--- menuctrl/encoder.py
+++ menuctrl/encoder.py
@@ -14,3 +14,6 @@ def test():
     GPIO.add_event_detect(25, Edge.BOTH, task1, bouncetime=30)
     GPIO.add_event_detect(14, Edge.BOTH, task2)
     GPIO.add_event_detect(26, Edge.BOTH, task2)
+    while True:
+        GPIO.get_line_value(25)
+        time.sleep(0.5)
--- menuctrl/gpio.py
+++ menuctrl/gpio.py
@@ -1,12 +1,13 @@
 import threading
 from datetime import timedelta
 import gpiod
-from gpiod.line import Bias, Direction
+from gpiod.line import Bias, Edge
 
 
 class AuriliaGPIO:
     def __init__(self):
         self.chip_path = "/dev/gpiochip0"
+        self.pin_values = {}
 
     def add_event_detect(self, pin_number, edge_detection, callback_function, bouncetime=0):
         thread = threading.Thread(target=self.poll_line_value, args=(pin_number, edge_detection,
@@ -21,7 +22,7 @@ class AuriliaGPIO:
             consumer="watch-line",
             config={
                 pin_number: gpiod.LineSettings(
-                    edge_detection=edge_detection,
+                    edge_detection=Edge.BOTH,
                     bias=Bias.PULL_UP,
                     debounce_period=timedelta(milliseconds=bouncetime),
                 )
@@ -30,9 +31,14 @@ class AuriliaGPIO:
             while True:
                 # Blocks until at least one event is available
                 for event in request.read_edge_events():
-                    pin_value =  request.get_value(pin_number)
-                    print(int(pin_value.value))
+                    pin_value =  request.get_value(pin_number).value
+                    self.pin_values.update(pin_number, pin_value)
                     callback_function()
 
+    def get_line_value(self, pin_number):
+        try:
+            return self.pin_values[pin_number]
+        except:
+            print("Error [gpio.py]: no value for pin_number stored")
 
 GPIO = AuriliaGPIO()
